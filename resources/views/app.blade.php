f4628e67db424de8adf54c01ff7feae5ff83b848
kennu156 <kenmarttipaju@gmail.com>
2026-02-13 00:30:17 +0200

    favicons added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,12 @@
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Burger Primo" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
@@ -31,11 +37,7 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <title inertia>{{ config('app.name', 'Burger Primo') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
